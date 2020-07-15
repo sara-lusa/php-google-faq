b51cbcdf0f28a39e581c933f27d732ee6d22fabe
Sara <saracorsob@gmail.com>
2020-07-15 17:19:44 +0200

    Modifiche css al main

--- index.php
+++ index.php
@@ -4,20 +4,20 @@ $array_qa = [
   [
     'question' => "Come state implementando la recente decisione della Corte di
       giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?",
-    'answer' => "La recente decisione della Corte di giustizia dell'Unione europea
+    'answer' => "La recente <span>decisione della Corte di giustizia dell'Unione europea</span>
       ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito
       che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google
       di rimuovere risultati relativi a chiavi di ricerca che includono il proprio
       nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati,
       irrilevanti o non più rilevanti, o eccessivi.
-
+      <br><br>
       Da quando questa decisione è stata pubblicata il 13 maggio 2014, abbiamo
       lavorato incessantemente per uniformarci a essa. Si tratta di una procedura
       complessa perché dobbiamo valutare ogni singola richiesta e effettuare un
       bilanciamento tra il diritto dell'individuo a controllare i suoi dati personali
       ed il diritto di tutti di conoscere e distribuire le informazioni.
-
-      Per presentare una richiesta di rimozione, compila questo modulo web.
+      <br><br>
+      Per presentare una richiesta di rimozione, compila questo <span>modulo web.</span>
       Riceverai una risposta automatica che conferma la ricezione della tua
       richiesta. Dopodiché valuteremo il tuo caso (tieni presente che potrebbe
       occorrere un po' di tempo perché abbiamo già ricevuto tante richieste simili).
@@ -30,14 +30,14 @@ $array_qa = [
       privata, potremmo non essere nella posizione giusta per prendere decisioni in merito
       al tuo caso. Se non sei d'accordo con la nostra valutazione, puoi rivolgerti all'Autorità
       garante per la protezione dei dati personali nel tuo paese.
-
+      <br><br>
       Nei prossimi mesi lavoreremo a stretto contatto con le autorità per la protezione
       dei dati e con altre autorità per perfezionare il nostro approccio. La decisione della
       CGUE rappresenta un cambiamento significativo per i motori di ricerca. Siamo
       preoccupati per le sue conseguenze, ma riteniamo anche che sia importante
       rispettare la decisione della Corte e ci stiamo adoperando per predisporre
       una procedura conforme alla legge.
-
+      <br><br>
       Quando cerchi un nome, potresti trovare una notifica in cui viene spiegato
       che i risultati potrebbero essere stati modificati nel rispetto delle leggi
       europee per la protezione dei dati. Mostriamo questa notifica agli utenti
@@ -49,7 +49,7 @@ $array_qa = [
     'answer' => "Sappiamo che la sicurezza e la privacy sono importanti per te
       e lo sono anche per noi. Per noi è imperativo offrire un'elevata sicurezza
       e assicurarti che le tue informazioni sono al sicuro e accessibili quando ne hai bisogno.
-
+      <br><br>
       Siamo costantemente al lavoro per garantire un'elevata sicurezza, proteggere la
       tua privacy e rendere Google ancora più efficace ed efficiente per te. Spendiamo
       centinaia di milioni di dollari ogni anno per la sicurezza e ci avvaliamo di
@@ -58,10 +58,10 @@ $array_qa = [
       la privacy di facile utilizzo come Google Dashboard, la verifica in due passaggi
       e Impostazioni annunci. Così, per quanto riguarda le informazioni che condividi
       con Google, hai il massimo controllo.
-
+      <br><br>
       Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere
-      te e la tua famiglia online, nel Centro Google per la sicurezza online.
-
+      te e la tua famiglia online, nel <span>Centro Google per la sicurezza online.</span>
+      <br><br>
       Scopri quali misure adottiamo per garantire protezione e sicurezza alle tue
       informazioni personali, lasciando a te il controllo."
   ],
@@ -71,14 +71,14 @@ $array_qa = [
       pubblicamente disponibili sul Web. I motori di ricerca non possono rimuovere
       i contenuti direttamente dai siti web, quindi rimuovere risultati di ricerca
       da Google non consente di rimuovere i contenuti dal Web. Se desideri rimuovere
-      qualcosa dal Web, devi contattare il webmaster del sito su cui sono pubblicati
+      qualcosa dal Web, devi <span>contattare il webmaster</span> del sito su cui sono pubblicati
       i contenuti e chiedergli di apportare una modifica. Inoltre, se, ai sensi delle
       leggi europee per la protezione dei dati, desideri richiedere la rimozione di
       determinate informazioni su di te visualizzate nei risultati di ricerca di Google,
-      fai clic qui. Una volta che i contenuti saranno stati rimossi e che Google avrà
+      <span>fai clic qui.</span> Una volta che i contenuti saranno stati rimossi e che Google avrà
       rilevato l'aggiornamento, le informazioni non verranno più visualizzate nei
       risultati di ricerca di Google. In caso di una richiesta di rimozione urgente,
-      è inoltre possibile visitare la nostra pagina di assistenza per avere ulteriori informazioni."
+      è inoltre possibile <span>visitare la nostra pagina di assistenza per avere ulteriori informazioni.</span>"
   ],
   [
     'question' => "Quando faccio clic sui risultati della Ricerca Google, le mie
@@ -86,12 +86,12 @@ $array_qa = [
     'answer' => "In alcuni casi sì. Quando fai clic su un risultato della Ricerca
       Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione
       anche l'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto
-      forma di URL referrer. Talvolta, l'URL della pagina dei risultati di ricerca
+      forma di <span>URL referrer.</span> Talvolta, l'URL della pagina dei risultati di ricerca
       potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca
       SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i
       termini di ricerca non vengono inviati come parte dell'URL negli URL referrer.
       Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser
-      meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili qui.
+      meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili <span>qui.</span>
       Le query di ricerca o le informazioni contenute nell'URL referrer potrebbero
       essere disponibili mediante Google Analytics o un'API (Application Programming
       Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative
@@ -107,6 +107,7 @@ $array_qa = [
     <meta charset="utf-8">
     <title>Google</title>
 
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
   </head>
   <body>
@@ -125,7 +126,7 @@ $array_qa = [
       <main>
         <ul>
           <?php foreach ($array_qa as $single_qa) { ?>
-            <li>
+            <li class="list-qa">
               <h2><?php echo $single_qa['question']; ?></h2>
               <p><?php echo $single_qa['answer'] ?></p>
             </li>
